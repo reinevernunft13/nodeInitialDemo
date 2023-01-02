765c9c90058b696fc46191e7c12ba5f5e5b15311
Cristina Carrillo <96286318+reinevernunft13@users.noreply.github.com>
2023-01-02 11:16:29 +0100

    Delete client directory

--- client/public/js/logout.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.button-logout').addEventListener('click', () => {
-    const leaveRoom = confirm('Are you sure that you want to log out?');
-    if(leaveRoom){
-        localStorage.clear();
-        sessionStorage.clear();
-        window.location.replace('./login.html');
-    }
-});
