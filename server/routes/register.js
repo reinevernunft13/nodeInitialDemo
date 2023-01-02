4a037c444f41315329dcc3a5ac2cba19da311da6
Cristina Carrillo <96286318+reinevernunft13@users.noreply.github.com>
2023-01-02 11:16:37 +0100

    Delete server directory

--- server/routes/register.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router()
-
-const register = require('../controllers/register');
-
-router.post( '/', register);
-
-module.exports = router;
